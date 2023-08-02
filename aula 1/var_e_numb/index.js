47433713e411d48b32a163a514a4f9253fe465fe
Ayrles Sales <ayrlesalespuc@gmail.com>
2023-08-02 14:41:59 -0300

    modificação nome da pasta apenas

--- aula 1/var_e_numb/index.js
+++
@@ -1,9 +0,0 @@
-let num1 = prompt('digite um número');
-let num2 = prompt('digite um outro número');
-
-num1 = parseFloat(num1);
-num2 = parseFloat(num2);
-
-let soma = num1 + num2;
-
-alert(`a soma deu ${soma}`)
