9e4bdf372ad3350667f67b0ae1748dbc01ce52af
Ayrles Sales <ayrlesalespuc@gmail.com>
2024-01-12 13:52:58 -0300

    get and set

--- objetosAndPrototypes/defineProperty.js
+++ objetosAndPrototypes/defineProperty.js
@@ -2,11 +2,21 @@ function Produto(nome, preco, estoque) {
     //this.nome = nome;
     //this.preco = preco;
     //this.estoque = estoque; 
+    let preco1 = preco;
 
     Object.defineProperty(this, 'preco', {
         enumerable: true,
-        value: preco,
-        writable: false
+        // value: preco,
+        // writable: false
+        get: function() {
+            return preco1
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number') {
+                throw new TypeError('bad value')
+            }
+            preco1 = valor;
+        } 
     });
 
     Object.defineProperties(this, {
@@ -24,6 +34,7 @@ function Produto(nome, preco, estoque) {
 }
 
 const p1 = new Produto('camiseta', 15, 30);
+console.log(p1);
 p1.preco = 10;
 p1.estoque = 29;
-console.log(p1);
+console.log(p1.preco);
